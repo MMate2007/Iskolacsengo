53f23e407c4598e9c7e77866d6090676de341dcb
Máté Menyhárt <mateka.mateka07@gmail.com>
2024-03-15 10:58:00 +0100

    Load programes every day

--- main.py
+++ main.py
@@ -3,16 +3,23 @@ import pygame
 import time
 
 pygame.init()
-
-with open("settings.json") as f:
-	settings = json.load(f)
+def loadprogrames():
+	global lastloaded, settings, programmes
+	lastloaded = time.localtime().tm_mday
+	with open("settings.json") as f:
+		settings = json.load(f)
 	
-with open(settings["programmeFilePath"]) as p:
-	programmes = json.load(p)
-	for prog in programmes:
-		prog["time"] = time.strptime(prog["time"], "%H:%M")
-		
+	with open(settings["programmeFilePath"]) as p:
+		programmes = json.load(p)
+		for prog in programmes:
+			prog["time"] = time.strptime(prog["time"], "%H:%M")
+
+loadprogrames()
+
 while True:
+	if lastloaded != time.localtime().tm_mday:
+		loadprogrames()
+	
 	for prog in programmes:
 		if prog["time"].tm_hour == time.localtime().tm_hour and prog["time"].tm_min == time.localtime().tm_min:
 			pygame.mixer.Sound(prog["file"]).play()
