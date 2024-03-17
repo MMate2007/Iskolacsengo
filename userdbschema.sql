7222f3e1c2f1c150b0d6bf799aaf8de193437c5e
Máté Menyhárt <mateka.mateka07@gmail.com>
2024-03-17 09:38:03 +0100

    GUI

--- userdbschema.sql
+++ userdbschema.sql
@@ -1,20 +1,17 @@
+DROP TABLE IF EXISTS users;
 CREATE TABLE users (
 	id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
 	username TEXT NOT NULL,
 	password TEXT NOT NULL
 );
+DROP TABLE IF EXISTS permissions;
 CREATE TABLE permissions (
 	id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
 	friendlyname TEXT NOT NULL,
 	description TEXT
 );
+DROP TABLE IF EXISTS userpermissions;
 CREATE TABLE userpermissions (
 	user_id INTEGER NOT NULL,
 	permission_id INTEGER NOT NULL
-);
-CREATE TABLE tokens (
-	id TEXT NOT NULL PRIMARY KEY,
-	user_id INTEGER NOT NULL,
-	created_at DATE NOT NULL DEFAULT (datetime('now', 'localtime')),
-	last_activity DATE NOT NULL DEFAULT (datetime('now', 'localtime'))
 );
