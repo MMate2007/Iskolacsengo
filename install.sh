328d5a87a2854931c9c7fd8bd4d21ef2fe91d794
MMate2007 <mateka.mateka07@gmail.com>
2024-04-24 17:37:36 +0200

    Restart on failure 30s, install Python and run initdbs

--- install.sh
+++ install.sh
@@ -1,22 +1,23 @@
 #!/bin/sh
 
-apt install python3-bcrypt python3-flask python3-flask-login python3-pygame
+apt install python3 python3-bcrypt python3-flask python3-flask-login python3-pygame -y
 dir=$PWD
+python initdbs.py
 cd /etc/systemd/system
 cat > iskolacsengo.service << END
 [Unit]
-Description=Start the main Python script of Iskolacsengo
+Description=Iskolacsengo
 After=network.target
 
 [Service]
 WorkingDirectory=$dir
 ExecStart=python $dir/main.py
-Restart=on-abnormal
+Restart=on-failure
+RestartSec=30
 
 [Install]
 WantedBy=default.target
 END
-
 systemctl daemon-reload
 systemctl enable iskolacsengo.service
 systemctl start iskolacsengo.service
