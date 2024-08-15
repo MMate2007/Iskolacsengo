15953f00e37fbf2ccee7fe1169bf1fd7bc4363aa
Menyhárt Máté <mateka.mateka07@gmail.com>
2024-08-15 15:04:52 +0200

    Update from CLI

--- updatehelper.py
+++ updatehelper.py
@@ -1,11 +1,11 @@
 import os
+import sys
 from shutil import rmtree
-from imexport import importfromfile
+from imexport import importfromfile, export
 
-os.system("systemctl stop iskolacsengo.service")
+export(sys.argv[1], "updateexport.json")
 os.system("git pull")
 os.system("python initdbs.py")
 importfromfile("updateexport.json")
 os.remove("updateexport.json")
 rmtree("__pycache__")
-os.system("systemctl start iskolacsengo.service")
